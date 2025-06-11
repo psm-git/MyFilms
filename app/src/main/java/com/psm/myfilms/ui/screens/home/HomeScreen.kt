cc6e0066c9d480d34c7109cc1808b639b673a18b
Pablo Santos <psm-github@outlook.com>
2025-06-11 12:06:41 +0200

    Adds geolocation
    The country will now be displayed on the top bar on the Home screen.

--- app/src/main/java/com/psm/myfilms/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/psm/myfilms/ui/screens/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.psm.myfilms.ui.screens.home
 
+import android.Manifest
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,28 +18,54 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.psm.myfilms.Movie
+import com.psm.myfilms.R
 import com.psm.myfilms.movies
+import com.psm.myfilms.ui.common.PermissionRequestEffect
+import com.psm.myfilms.ui.common.getRegion
 import com.psm.myfilms.ui.screens.Screen
+import kotlinx.coroutines.launch
 
 const val HOME_SCREEN_ROUTE = "home"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(onMovieClicked: (Movie) -> Unit) {
+
+    val context = LocalContext.current
+    var appBarTitle by remember { mutableStateOf(context.getString(R.string.app_name)) }
+    val coroutineScope = rememberCoroutineScope()
+
+    PermissionRequestEffect(permission = Manifest.permission.ACCESS_COARSE_LOCATION) { granted ->
+        if (granted) {
+            coroutineScope.launch {
+                val region = context.getRegion()
+                appBarTitle = "$appBarTitle ($region)"
+            }
+        } else {
+            appBarTitle = "$appBarTitle (Permission denied)"
+        }
+    }
+
     Screen {
         val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
         Scaffold(
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
                 TopAppBar(
-                    title = { Text("Movies") },
+                    title = { Text(text = appBarTitle) },
                     scrollBehavior = scrollBehavior
                 )
             },
