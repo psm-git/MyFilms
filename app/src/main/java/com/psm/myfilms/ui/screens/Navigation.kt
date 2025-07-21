de329bc059977e7392b03d9abc025fb41034ebb5
Pablo Santos <psm-github@outlook.com>
2025-07-21 13:34:57 +0200

    Implements type-safe navigation
    Instead of using strings for routes and arguments, now the Navigation component uses a type-safe system.

--- app/src/main/java/com/psm/myfilms/ui/screens/Navigation.kt
+++ app/src/main/java/com/psm/myfilms/ui/screens/Navigation.kt
@@ -2,16 +2,22 @@ package com.psm.myfilms.ui.screens
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.psm.myfilms.ui.screens.detail.DETAIL_SCREEN_ROUTE
+import androidx.navigation.toRoute
 import com.psm.myfilms.ui.screens.detail.DetailScreen
 import com.psm.myfilms.ui.screens.detail.DetailViewModel
-import com.psm.myfilms.ui.screens.home.HOME_SCREEN_ROUTE
 import com.psm.myfilms.ui.screens.home.HomeScreen
+import kotlinx.serialization.Serializable
+
+object NavScreen {
+    @Serializable
+    object Home
+
+    @Serializable
+    data class Detail(val movieId: Int)
+}
 
 @Composable
 fun Navigation() {
@@ -19,18 +25,15 @@ fun Navigation() {
 
     NavHost(
         navController = navController,
-        startDestination = HOME_SCREEN_ROUTE
+        startDestination = NavScreen.Home
     ) {
-        composable(route = HOME_SCREEN_ROUTE) {
+        composable<NavScreen.Home> {
             HomeScreen(
-                onMovieClicked = { navController.navigate("$DETAIL_SCREEN_ROUTE/${it.id}") }
+                onMovieClicked = { navController.navigate(NavScreen.Detail(it.id)) }
             )
         }
-        composable(
-            route = "$DETAIL_SCREEN_ROUTE/{movieId}",
-            arguments = listOf(navArgument("movieId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val movieId = requireNotNull(backStackEntry.arguments?.getInt("movieId"))
+        composable<NavScreen.Detail> { backStackEntry ->
+            val movieId = backStackEntry.toRoute<NavScreen.Detail>().movieId
             DetailScreen(
                 viewModel = viewModel { DetailViewModel(movieId) },
                 onBackClicked = { navController.popBackStack() }
